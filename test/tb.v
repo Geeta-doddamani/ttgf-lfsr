122dc5c90b7417d0298c1133c989706e35d48dd4
Geeta-doddamani <128340936+Geeta-doddamani@users.noreply.github.com>
2026-05-29 14:28:41 +0530

    Refactor testbench for LFSR module

--- test/tb.v
+++ test/tb.v
@@ -1,31 +1,44 @@
-`timescale 1ns/1ps
+`default_nettype none
+`timescale 1ns / 1ps
 
-module lfsr_tb;
+module tb ();
 
-reg clk, reset;
-wire [3:0] lfsr_out;
+  initial begin
+    $dumpfile("tb.fst");
+    $dumpvars(0, tb);
+    #1;
+  end
 
-lfsr_4bit DUT (
-    .clk(clk),
-    .reset(reset),
-    .lfsr(lfsr_out)
-);
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
 
-initial begin
-    clk = 0;
-    forever #5 clk = ~clk;   // 100 MHz clock
-end
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
-initial begin
-    reset = 1;
-    #20 reset = 0;
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
 
-    #200 $finish;
-end
+  tt_um_geeta_doddamani_lfsr user_project (
 
-initial begin
-    $monitor("Time=%0t | clk=%b | reset=%b | lfsr_out=%b",
-              $time, clk, reset, lfsr_out);
-end
+`ifdef GL_TEST
+      .VPWR(VPWR),
+      .VGND(VGND),
+`endif
+
+      .ui_in  (ui_in),
+      .uo_out (uo_out),
+      .uio_in (uio_in),
+      .uio_out(uio_out),
+      .uio_oe (uio_oe),
+      .ena    (ena),
+      .clk    (clk),
+      .rst_n  (rst_n)
+  );
 
 endmodule
